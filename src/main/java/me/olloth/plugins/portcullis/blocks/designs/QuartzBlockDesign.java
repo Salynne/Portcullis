aea6615462d4644c7cb6a6fe64533946d3079a05
Olloth <olloth@gmail.com>
2011-10-07 06:03:41 -0400

    No, you're a dummy, dummy!

--- src/main/java/me/olloth/plugins/portcullis/blocks/designs/QuartzBlockDesign.java
+++ src/main/java/me/olloth/plugins/portcullis/blocks/designs/QuartzBlockDesign.java
@@ -52,61 +52,61 @@ public class QuartzBlockDesign extends GenericBlockDesign {
 
 		Quad top1 = new Quad(5, subTex);
 		top1.addVertex(0, getX(0), 1F, getZ(0));
-		top1.addVertex(1, 0.5F, 1.5F, 0.5F); // Dummy vertex for a triangle
+		top1.addVertex(1, 0.5F, 1.5F, 0.5F); 
 		top1.addVertex(2, getX(72), 1F, getZ(72));
 		top1.addVertex(3, getX(0), 1F, getZ(0));
 
 		Quad top2 = new Quad(6, subTex);
 		top2.addVertex(0, getX(72), 1F, getZ(72));
-		top2.addVertex(1, 0.5F, 1.5F, 0.5F); // Dummy vertex for a triangle
+		top2.addVertex(1, 0.5F, 1.5F, 0.5F); 
 		top2.addVertex(2, getX(72 * 2), 1F, getZ(72 * 2));
 		top2.addVertex(3, getX(72), 1F, getZ(72));
 
 		Quad top3 = new Quad(7, subTex);
 		top3.addVertex(0, getX(72 * 2), 1F, getZ(72 * 2));
-		top3.addVertex(1, 0.5F, 1.5F, 0.5F); // Dummy vertex for a triangle
+		top3.addVertex(1, 0.5F, 1.5F, 0.5F); 
 		top3.addVertex(2, getX(72 * 3), 1F, getZ(72 * 3));
 		top3.addVertex(3, getX(72 * 2), 1F, getZ(72 * 2));
 
 		Quad top4 = new Quad(8, subTex);
 		top4.addVertex(0, getX(72 * 3), 1F, getZ(72 * 3));
-		top4.addVertex(1, 0.5F, 1.5F, 0.5F); // Dummy vertex for a triangle
+		top4.addVertex(1, 0.5F, 1.5F, 0.5F); 
 		top4.addVertex(2, getX(72 * 4), 1F, getZ(72 * 4));
 		top4.addVertex(3, getX(72 * 3), 1F, getZ(72 * 3));
 
 		Quad top5 = new Quad(9, subTex);
 		top5.addVertex(0, getX(72 * 4), 1F, getZ(72 * 4));
-		top5.addVertex(1, 0.5F, 1.5F, 0.5F); // Dummy vertex for a triangle
+		top5.addVertex(1, 0.5F, 1.5F, 0.5F); 
 		top5.addVertex(2, getX(0), 1F, getZ(0));
 		top5.addVertex(3, getX(72 * 4), 1F, getZ(72 * 4));
 
 		Quad bottom1 = new Quad(10, subTex);
 		bottom1.addVertex(0, getX(72), 0F, getZ(72));
-		bottom1.addVertex(1, 0.5F, 0F, 0.5F); // Dummy vertex for a triangle
+		bottom1.addVertex(1, 0.5F, 0F, 0.5F); 
 		bottom1.addVertex(2, getX(0), 0F, getZ(0));
 		bottom1.addVertex(3, getX(0), 0F, getZ(0));
 
 		Quad bottom2 = new Quad(11, subTex);
 		bottom2.addVertex(0, getX(72 * 2), 0F, getZ(72 * 2));
-		bottom2.addVertex(1, 0.5F, 0F, 0.5F); // Dummy vertex for a triangle
+		bottom2.addVertex(1, 0.5F, 0F, 0.5F); 
 		bottom2.addVertex(2, getX(72), 0F, getZ(72));
 		bottom2.addVertex(3, getX(72), 0F, getZ(72));
 
 		Quad bottom3 = new Quad(12, subTex);
 		bottom3.addVertex(0, getX(72 * 3), 0F, getZ(72 * 3));
-		bottom3.addVertex(1, 0.5F, 0F, 0.5F); // Dummy vertex for a triangle
+		bottom3.addVertex(1, 0.5F, 0F, 0.5F); 
 		bottom3.addVertex(2, getX(72 * 2), 0F, getZ(72 * 2));
 		bottom3.addVertex(3, getX(72 * 2), 0F, getZ(72 * 2));
 
 		Quad bottom4 = new Quad(13, subTex);
 		bottom4.addVertex(0, getX(72 * 4), 0F, getZ(72 * 4));
-		bottom4.addVertex(1, 0.5F, 0F, 0.5F); // Dummy vertex for a triangle
+		bottom4.addVertex(1, 0.5F, 0F, 0.5F); 
 		bottom4.addVertex(2, getX(72 * 3), 0F, getZ(72 * 3));
 		bottom4.addVertex(3, getX(72 * 3), 0F, getZ(72 * 3));
 
 		Quad bottom5 = new Quad(14, subTex);
 		bottom5.addVertex(0, getX(0), 0F, getZ(0));
-		bottom5.addVertex(1, 0.5F, 0F, 0.5F); // Dummy vertex for a triangle
+		bottom5.addVertex(1, 0.5F, 0F, 0.5F); 
 		bottom5.addVertex(2, getX(72 * 4), 0F, getZ(72 * 4));
 		bottom5.addVertex(3, getX(72 * 4), 0F, getZ(72 * 4));
 
