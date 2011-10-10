e6e3b0f52ca803a4f5ce997e88ce84cd3405ca34
Olloth <olloth@gmail.com>
2011-10-09 21:29:37 -0400

    Reverted drop code, moved it to the API to default. Removed texture pack.

--- src/main/java/me/olloth/plugins/portcullis/blocks/BlueQuartz.java
+++ src/main/java/me/olloth/plugins/portcullis/blocks/BlueQuartz.java
@@ -3,8 +3,6 @@ package me.olloth.plugins.portcullis.blocks;
 import me.olloth.plugins.portcullis.Portcullis;
 import me.olloth.plugins.portcullis.blocks.designs.QuartzBlockDesign;
 
-import org.getspout.spoutapi.SpoutManager;
-import org.getspout.spoutapi.inventory.ItemManager;
 import org.getspout.spoutapi.material.block.GenericCustomBlock;
 
 public class BlueQuartz extends GenericCustomBlock {
@@ -12,8 +10,6 @@ public class BlueQuartz extends GenericCustomBlock {
 	public BlueQuartz() {
 		super(Portcullis.instance, "Blue Quartz", false);
 		this.setBlockDesign(new QuartzBlockDesign(4));
-		ItemManager im = SpoutManager.getItemManager();
-		im.registerItemDrop(this, im.getCustomItemStack(this, 1));
 	}
 
 }
--- src/main/java/me/olloth/plugins/portcullis/blocks/Quartz.java
+++ src/main/java/me/olloth/plugins/portcullis/blocks/Quartz.java
@@ -3,8 +3,6 @@ package me.olloth.plugins.portcullis.blocks;
 import me.olloth.plugins.portcullis.Portcullis;
 import me.olloth.plugins.portcullis.blocks.designs.QuartzBlockDesign;
 
-import org.getspout.spoutapi.SpoutManager;
-import org.getspout.spoutapi.inventory.ItemManager;
 import org.getspout.spoutapi.material.block.GenericCustomBlock;
 
 public class Quartz extends GenericCustomBlock {
@@ -12,8 +10,6 @@ public class Quartz extends GenericCustomBlock {
 	public Quartz() {
 		super(Portcullis.instance, "Quartz", false);
 		this.setBlockDesign(new QuartzBlockDesign(3));
-		ItemManager im = SpoutManager.getItemManager();
-		im.registerItemDrop(this, im.getCustomItemStack(this, 1));
 	}
 
 }
